06a612a6553557029b0253cccaa6e4086d3741f4
Pat0h <patrickngatia14@gmail.com>
2023-12-03 22:16:15 +0300

    customer

--- lib/customer.py
+++ lib/customer.py
@@ -1,6 +1,7 @@
 class Customer:
     all_customers = []
-    def __init__(self,given_name,family_name):
+
+    def __init__(self, given_name, family_name):
         self.given_name = given_name
         self.family_name = family_name
         self.all_customers.append(self)
@@ -14,6 +15,18 @@ class Customer:
     def full_name(self):
         return f'{self.given_name} {self.family_name}'
 
+    @classmethod
+    def all(cls):
+        return cls.all_customers
 
-customer1= Customer('Patrick','Ngatia')
-customer2= Customer('Purity','Ngatia')
+
+# Tests
+customer1 = Customer('Patrick', 'Ngatia')
+customer2 = Customer('Purity', 'Ngatia')
+
+# print(customer1.full_name())
+# print(customer2.full_name())
+
+all_customers = Customer.all()
+for Customer in all_customers:
+    print(Customer.full_name())
